9a97cad285826c9a4e85f15db8cb8824ef6298f6
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2023-11-21 13:07:19 -0300

    Agregar archivos de proyecto.

---
+++ RecuParcialProgI.Windows/frmFiguraAE.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RecuParcialProgI.Windows
+{
+    partial class frmFiguraAE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtLado = new TextBox();
+            errorProvider1 = new ErrorProvider(components);
+            btnOK = new Button();
+            btnCancelar = new Button();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Lado:";
+            // 
+            // txtLado
+            // 
+            txtLado.Location = new Point(128, 51);
+            txtLado.Name = "txtLado";
+            txtLado.Size = new Size(100, 23);
+            txtLado.TabIndex = 1;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // btnOK
+            // 
+            btnOK.Image = Properties.Resources.ok_24px;
+            btnOK.Location = new Point(43, 230);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(92, 67);
+            btnOK.TabIndex = 2;
+            btnOK.Text = "OK";
+            btnOK.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Image = Properties.Resources.cancel_24px;
+            btnCancelar.Location = new Point(355, 230);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(92, 67);
+            btnCancelar.TabIndex = 2;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmFiguraAE
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(497, 358);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnOK);
+            Controls.Add(txtLado);
+            Controls.Add(label1);
+            Name = "frmFiguraAE";
+            Text = "frmFiguraAE";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtLado;
+        private ErrorProvider errorProvider1;
+        private Button btnCancelar;
+        private Button btnOK;
+    }
+}
---
+++ RecuParcialProgI.Windows/frmFiguraAE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecuParcialProgI.Windows
+{
+    public partial class frmFiguraAE : Form
+    {
+        public frmFiguraAE()
+        {
+            InitializeComponent();
+        }
+    }
+}
